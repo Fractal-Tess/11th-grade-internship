9cda68cc0357b1fed0463c25aef2a49e31654d06
Vilian <vi.gecoding@gmail.com>
2021-04-08 22:59:20 +0300

    Added conifgs to the flask server

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
-
+PORT = 80
+HOST = "0.0.0.0"
 @app.route('/')
 def index():
     return render_template("index.html", title="Index")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=PORT, host=HOST)
     
