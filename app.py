6e7c4263d7a00d77a685d7153c6b960018fd8c1e
Vilian <vi.gecoding@gmail.com>
2021-04-08 23:04:20 +0300

    moved port to 5000

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
-PORT = 80
+
+#Configs
+PORT = 5000
 HOST = "0.0.0.0"
+
 @app.route('/')
 def index():
     return render_template("index.html", title="Index")
