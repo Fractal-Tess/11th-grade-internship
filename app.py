d53891532626147b29c0f3c91493ce01b3ebdd25
Vilian <vi.gecoding@gmail.com>
2021-04-13 08:58:00 +0300

    Added 2 more pages and their respective routes. Added some additional styling

--- app.py
+++ app.py
@@ -6,6 +6,17 @@ app = Flask(__name__)
 PORT = 5000
 HOST = "0.0.0.0"
 
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", title="Home")
+
+@app.route('/openCV-stream')
+def openCV_stream():
+    return render_template("opencv_stream.html", title="OpenCV Stream")
+
+
 @app.route('/frontend-face-api')
 def face_api():
     return render_template("faceapi.html", title="Index")
